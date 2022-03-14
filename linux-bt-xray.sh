4927e1d33860b1f72d6b4531aabcc6be40b01b74
darkvanming <86154358+darkvanming@users.noreply.github.com>
2022-03-14 20:52:48 +0800

    Create linux-bt-xray.sh

---
+++ linux-bt-xray.sh
@@ -0,0 +1,3 @@
+apt install curl 
+bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+wget -O install.sh http://v7.hostcli.com/install/install-ubuntu_6.0.sh && sudo bash install.sh
